1d59db19e534aefdf262bf87f7a0b72f54335463
anhtvmath <85474630+anhtvmath@users.noreply.github.com>
2021-06-07 18:35:24 +0700

    Add files via upload

--- validate.js
+++ validate.js
@@ -1,18 +1,20 @@
-﻿   function validate() {
+﻿    function validate() {
     var tensinhvien = document.getElementById("studentname").value;
-    var masinhvien = document.getElementById("studentid").value;   
-    if (tensinhvien == null || tensinhvien == "")
+    var masinhvien = document.getElementById("studentid").value;
+    username = new Array("Trần Việt Anh","u49","u50");
+    password = new Array("12345","p49","p50");
+    customer = new Array("Lê Anh","c2","c3");
+    if (tensinhvien == customer[0] || tensinhvien == customer[1] || tensinhvien == customer[2])
     {
-    alert("Xin vui nhập họ và tên!");
-    } 
-    if (masinhvien == null || masinhvien == "")
-    {
-    alert("Xin vui lòng nhập mã sinh viên!");
-    } 
-    if (tensinhvien != null & tensinhvien != "" & masinhvien != null & masinhvien != "") {    
+    alert("Tài khoản của bạn đã hết hạn sử dụng. Xin vui lòng đăng ký để sử dụng phần mềm!");
+    }
+    if (tensinhvien == username[0] && masinhvien == password[0] || 
+    tensinhvien == username[1] && masinhvien == password[1] || 
+    tensinhvien == username[2] && masinhvien == password[2]) {
     document.getElementById("formdangnhap").style.display="none";
     document.getElementById("tieudehuongdan").style.display="block";
     document.getElementById("noidunghuongdan").style.display="block";
     document.getElementById("lambai").style.display="block";
     }
+    else alert("Tên đăng nhập hoặc mật khẩu không đúng! Xin vui lòng thử lại!");
     }
