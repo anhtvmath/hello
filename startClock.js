2ced1755b10becfa3c6c21927bbc2386ce609fc9
anhtvmath <85474630+anhtvmath@users.noreply.github.com>
2021-06-07 18:04:14 +0700

    Add files via upload

---
+++ startClock.js
@@ -0,0 +1,20 @@
+﻿    function startClock() {
+    var seconds = 30;
+      setInterval(function() {
+        var minutes = Math.round((seconds - 30)/60);
+        var remainingSeconds = seconds % 60;
+        if (remainingSeconds < 10) {
+            remainingSeconds = "0" + remainingSeconds;  
+        }
+        document.getElementById("countdown").innerHTML = minutes + ":" + remainingSeconds;
+        if (seconds == 0) {
+            document.getElementById("countdown").innerHTML = "đã hết!";
+            document.getElementById("buttonxemketqua").click();
+            for (var i=1; i<=document.getElementById("show").children.length; i++) {
+            document.getElementById("show"+String(i)).style.display="block";
+            }
+        } else {
+            seconds--;
+        }
+        }, 1000);
+        }
