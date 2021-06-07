bf33edd6ba4028f4682dc6b087ebe6a4cade10cd
anhtvmath <85474630+anhtvmath@users.noreply.github.com>
2021-06-07 12:39:14 +0700

    Add files via upload

---
+++ validate.js
@@ -0,0 +1,18 @@
+﻿   function validate() {
+    var tensinhvien = document.getElementById("studentname").value;
+    var masinhvien = document.getElementById("studentid").value;   
+    if (tensinhvien == null || tensinhvien == "")
+    {
+    alert("Xin vui nhập họ và tên!");
+    } 
+    if (masinhvien == null || masinhvien == "")
+    {
+    alert("Xin vui lòng nhập mã sinh viên!");
+    } 
+    if (tensinhvien != null & tensinhvien != "" & masinhvien != null & masinhvien != "") {    
+    document.getElementById("formdangnhap").style.display="none";
+    document.getElementById("tieudehuongdan").style.display="block";
+    document.getElementById("noidunghuongdan").style.display="block";
+    document.getElementById("lambai").style.display="block";
+    }
+    }
